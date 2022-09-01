d5651e830fe1baf8ea8f14e64e01867bda033ab3
Prs2630 <poojakinholkar1998@gmail.com>
2022-09-01 07:41:14 +0530

    first commit

---
+++ src/components/Formcomponents.js
@@ -0,0 +1,25 @@
+
+
+
+
+
+
+
+
+import React from 'react'
+import Grid from '@mui/material/Grid';
+
+const Formcomponents = () => {
+    return (
+        <div>
+            <Grid container>
+                <Grid item xs={12} sm={7}>
+                    <h1>form workss</h1>
+                </Grid>
+
+            </Grid>
+        </div>
+    )
+}
+
+export default Formcomponents;
